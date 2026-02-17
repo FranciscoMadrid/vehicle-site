be2618daa24d176f9093a21987a6e8a4cb22672d
FranciscoMadrid <ffms-a@outlook.com>
2026-02-16 23:54:30 -0600

    Fix: Syncing folder casing with GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,29 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  outDir: './dist',
+  output: 'static',
   devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
         "@": "/src"
+      }
+    },
+    ssr: {
+      noExternal: ['primereact']
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          }
+        }
       }
     }
   }
